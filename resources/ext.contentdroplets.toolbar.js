c0342a955eb1f559ba1ee798c00e334882aa1bf5
Mschlindwein <schlindwein@hallowelt.com>
2024-04-04 08:53:26 +0200

    Add missing message for toolbar icon
    ERM36681
    
    [4.4.x]
    
    Change-Id: I65e211a84daa6dec0866560a764151143f853914

--- resources/ext.contentdroplets.toolbar.js
+++ resources/ext.contentdroplets.toolbar.js
@@ -11,7 +11,7 @@ ve.ui.ContentDropletTool = function VeUiContentDropletTool( toolGroup ) {
 OO.inheritClass( ve.ui.ContentDropletTool, ve.ui.ToolbarDialogTool );
 ve.ui.ContentDropletTool.static.name = 'contentdroplet-toolbar';
 ve.ui.ContentDropletTool.static.group = 'dialog';
-ve.ui.ContentDropletTool.static.title = 'Content droplet';
+ve.ui.ContentDropletTool.static.title = mw.message( 'contentdroplets-toolbar-icon-title' ).text();
 ve.ui.ContentDropletTool.static.icon = 'contentDroplet';
 ve.ui.ContentDropletTool.static.autoAddToCatchall = false;
 ve.ui.ContentDropletTool.static.autoAddToGroup = false;
