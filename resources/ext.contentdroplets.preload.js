a8f51d8ed64770926ff1fc04af6c542022ce624f
d.savuljesku <d.savuljesku@gmail.com>
2025-02-28 10:50:44 +0100

    Make sure BSVEC modules are loaded before loading droplets
    I know, this is not nice, as ContentDroplets is generic, but there is just
    no way to reliably load BSVEC tags modules which most of droplets depend on
    before trying to initialize the droplets
    
    Proper way would be for actual droplet implementations to depend on VEC module,
    but since we dont want to introduce a hard dependency to BSVEC in those extensions
    that is not possible without breaking CI
    
    [5.x]
    
    ERM41019
    
    Change-Id: I6b95649ba91760f7308b7b1b88e246bcd7ea5b97

--- resources/ext.contentdroplets.preload.js
+++ resources/ext.contentdroplets.preload.js
@@ -1,25 +1,36 @@
 // Register models for droplets, so that VE recognizes them on load
-// At alter point, full droplet data will be loaded, which will register inspectors
+// At later point, full droplet data will be loaded, which will register inspectors
 // Reason it is not done here is messages, which cannot be loaded through load.php entrypoint
 const definitions = require( './dropletDefinitions.json' );
 
-for ( const key in definitions ) {
-	// eslint-disable-next-line no-prototype-builtins
-	if ( !definitions.hasOwnProperty( key ) ) {
-		continue;
-	}
-	let config = definitions[ key ];
-	config.key = key;
-	config.preload = true;
-	const dropletClass = ext.contentdroplets.registry.lookup( key );
-	if ( !dropletClass ) {
-		if ( config.content.indexOf( 'target' ) !== -1 ) {
-			new ext.contentdroplets.object.TransclusionDroplet( config );
-			return;
+function preload() {
+	for ( const key in definitions ) {
+		// eslint-disable-next-line no-prototype-builtins
+		if ( !definitions.hasOwnProperty( key ) ) {
+			continue;
 		}
-		new ext.contentdroplets.object.Droplet( config );
-	} else {
-		// eslint-disable-next-line new-cap
-		new dropletClass( config );
+		const config = definitions[ key ];
+		config.key = key;
+		config.preload = true;
+		const dropletClass = ext.contentdroplets.registry.lookup( key );
+		if ( !dropletClass ) {
+			if ( config.content.indexOf( 'target' ) !== -1 ) {
+				new ext.contentdroplets.object.TransclusionDroplet( config );
+				return;
+			}
+			new ext.contentdroplets.object.Droplet( config );
+		} else {
+			// eslint-disable-next-line new-cap
+			new dropletClass( config );
+		}
 	}
+}
+
+if ( mw.loader.getState( 'ext.bluespice.visualEditorConnector.tags' ) === 'registered' ) {
+	// If BlueSpiceVisualEditorConnector is enabled (and not yet loaded), load it first
+	mw.loader.using( 'ext.bluespice.visualEditorConnector.tags', function() {
+		preload();
+	} );
+} else {
+	preload();
 }
